7a7b76bfa601c7c523b8c5f448018db2924c4c1d
Harm Aarts <harmaarts@gmail.com>
2015-02-02 13:20:51 +0100

    Removes commented out code.

--- result.go
+++ result.go
@@ -28,31 +28,6 @@ func (r *Result) Error() string {
 
 func newResult(resp *Response, err error) *Result {
 	pageable := pageable{}
-	// if resp != nil {
-	// 	fillPageable(&pageable, resp.MediaHeader)
-	// }
 
 	return &Result{Response: resp, pageable: pageable, Err: err}
 }
-
-// func fillPageable(pageable *pageable, header *mediaheader.MediaHeader) {
-// 	if link, ok := header.Relations["next"]; ok {
-// 		l := Hyperlink(link)
-// 		pageable.NextPage = &l
-// 	}
-//
-// 	if link, ok := header.Relations["prev"]; ok {
-// 		l := Hyperlink(link)
-// 		pageable.PrevPage = &l
-// 	}
-//
-// 	if link, ok := header.Relations["first"]; ok {
-// 		l := Hyperlink(link)
-// 		pageable.FirstPage = &l
-// 	}
-//
-// 	if link, ok := header.Relations["last"]; ok {
-// 		l := Hyperlink(link)
-// 		pageable.LastPage = &l
-// 	}
-// }
