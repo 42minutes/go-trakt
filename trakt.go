c1705d4457cf2151efb39f6b5f6366e8abc815b2
Paris Goudas <paris@gordius.gr>
2015-09-27 16:33:23 +0300

    Change TraktAPIURL

--- trakt.go
+++ trakt.go
@@ -1,7 +1,7 @@
 package trakt
 
 const (
-	TraktAPIURL      = "https://api.trakt.tv"
+	TraktAPIURL      = "https://api-v2launch.trakt.tv"
 	TraktAPIVersion  = "2"
 	UserAgent        = "Trakt Go " + Version
 	Version          = "0.0.1"
